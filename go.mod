2c03071d5e76d4094254f6f8e929850fb102da85
yueban <yuebanlaosiji@outlook.com>
2021-07-12 14:50:20 +0800

    update test

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.16
 
 require (
 	github.com/gofrs/uuid v3.2.0+incompatible
-	github.com/shopspring/decimal v0.0.0-20200227202807-02e2044944cc
 	gorm.io/driver/postgres v1.1.0
 	gorm.io/gorm v1.21.11
 )
--- sample/demo_test.go
+++ sample/demo_test.go
@@ -1,1 +1,54 @@
 package sample
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+
+	"gorm.io/gorm/clause"
+)
+
+func RandomString(n int) string {
+
+	var letter = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	b := make([]rune, n)
+	length := len(letter)
+
+	for i := range b {
+		b[i] = letter[rand.Intn(length)]
+	}
+
+	return string(b)
+}
+
+func NonceStr() string {
+
+	return RandomString(32)
+}
+
+func Test_demo(t *testing.T) {
+	_db := GetDB()
+
+	for {
+
+		_new_user := UserInfo{
+			Name: NonceStr(),
+		}
+
+		// 更新用户字段
+		_db.Clauses(clause.OnConflict{
+			Columns:   []clause.Column{{Name: "id"}},              // key colume
+			DoUpdates: clause.AssignmentColumns([]string{"name"}), // column needed to be updated
+		}).Create(&_new_user)
+
+		db.Commit()
+
+		time.Sleep(time.Second * 10)
+
+		fmt.Println("db commit")
+
+	}
+
+}
